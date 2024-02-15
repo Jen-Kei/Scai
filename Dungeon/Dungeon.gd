190edb3b400c72866a55ee4aadfe9b17554af22c
Jenny Thao Huynh C22448184 <c22448184@mytudublin.ie>
2024-02-15 13:40:34 +0000

    Room outline generation done

--- Dungeon/Dungeon.gd
+++ Dungeon/Dungeon.gd
@@ -5,10 +5,11 @@ var Room = preload("Room/Room.tscn")
 
 # VARIABLES
 @export var tileSize = 16
-@export var numRooms = 50
+@export var numRooms = 30
 @export var minSize = 4
 @export var maxSize = 10
-@export var horizontalSpread = 100 
+@export var horizontalSpread = 50 
+@export var filter = 0.5 # filter var to delete a certain amount of generated rooms
 
 
 # Called when the node enters the scene tree for the first time.
@@ -34,6 +35,16 @@ func make_rooms():
 		roomNo.make_room(pos, Vector2(width, height) * tileSize) # call function to generate room
 		$Rooms.add_child(roomNo) # add newly generated room to rooms container
 	
+		# wait for physics engine to stop spreading apart the rooms
+		await(get_tree().create_timer(1.1).timeout) 
+		# Loop through every child in the room container and if randomly gen number is less then filter number
+		# Delete child
+		for room in $Rooms.get_children():
+			if randf() < filter:
+				room.queue_free()
+			else:
+				room.freeze = true # Change child so it can't be moved anymore
+
 func _draw():
 	# Draw the outline for the rooms
 	for room in $Rooms.get_children():
