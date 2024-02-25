4c7fe792b3dcc5c41a0685de0f7511381df9b759
Jenny Thao Huynh C22448184 <c22448184@mytudublin.ie>
2024-02-25 20:26:02 +0000

    Reference stat bank instead of hard coding

--- StaminaBar/StaminaBar.gd
+++ StaminaBar/StaminaBar.gd
@@ -1,7 +1,11 @@
 extends TextureProgressBar
 
 @export var player: Player
-@onready var staminaPerSecond = 1.0
+
+@onready var staminaBar = $StaminaBar/TextureProgressBar
+@onready var statBank = get_tree().root.get_child(0).get_child(0).get_node("StatBank")
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,13 +13,18 @@ func _ready():
 	player.stamina_increase.connect(_on_player_stamina_increase)
 	#value = player.stamina
 
+
 # Update stamina value
 func _on_player_stamina_decrease():
-	player.stamina -= 5
-	player.stamina = clamp(player.stamina, 0, 500)
+	var staminaCapacity = statBank.stamina_capacity
+	var staminaGain = statBank.stamina_gain
+	player.stamina -= staminaGain
+	player.stamina = clamp(player.stamina, 0, staminaCapacity)
 	value = player.stamina
 
 func _on_player_stamina_increase():
-	player.stamina += staminaPerSecond
-	player.stamina = clamp(player.stamina, 0, 500)
+	var staminaCapacity = statBank.stamina_capacity
+	var staminaGain = statBank.stamina_gain
+	player.stamina += staminaGain
+	player.stamina = clamp(player.stamina, 0, staminaCapacity)
 	value = player.stamina
