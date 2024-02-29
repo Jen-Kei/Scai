8a9a1b314d266960f7e46a8df59dfff0f43a456d
chubbyyb <keithsalhani7@gmail.com>
2024-02-29 02:31:59 +0000

    modified:   Enemies/Catboom/CatBoom.gd

--- Enemies/Catboom/CatBoom.gd
+++ Enemies/Catboom/CatBoom.gd
@@ -1,64 +1,23 @@
 extends CharacterBody2D
 
 @onready var anim = $AnimationPlayer
-@onready var player = get_parent().get_node("Player")
-@onready var selfSprite = $Sprite2D
-@onready var timer = $Timer
-@onready var _particle = preload("res://Enemies/Catboom/Assets/death.tscn")
-@onready var nav = $Navigation/NavigationAgent2D
+@onready var player = $Player
+var speed = 100
 
-var RADIUS = 180
-var EXPLODEWHEN = 50
-var exploded = false
-var acceleration = 7
-
-var speed = 50
-var vector_to_player
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	anim.play("idle")
+	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	# Calculate the vector from the enemy to the player
-	vector_to_player = player.position - self.position
-	var direction = Vector2.ZERO
-	direction = nav.get_next_path_position() - global_position
-	direction = direction.normalized()
-	# Check if the player is to the left or right
-	if player.position.distance_to(self.position) < RADIUS and !exploded:
-		#self.position += (player.position - self.position).normalized() * delta * speed
-		nav.target_position = player.position
-		velocity = velocity.lerp(direction * speed, acceleration * delta)
-		if vector_to_player.x < 0:
-			selfSprite.flip_h = true
-			anim.play("jog")
-		else:
-			selfSprite.flip_h = false
-			anim.play("jog")
-		
-		if player.position.distance_to(self.position) < EXPLODEWHEN and !exploded:
-			exploded = true
-			explode()
-		
-	if exploded:
-		self.position += (player.position - self.position).normalized() * delta * speed * 2
+	if player.position.distance_to(self.position) < 450:
+		self.position += (player.position - self.position).normalized() * delta * speed
 
-	move_and_slide()
-		
-
-func explode():
-	timer.start()
-	anim.play("jump")
 
 func _on_timer_timeout():
-	var particle = _particle.instantiate()
-	particle.position = global_position
-	particle.rotation = global_rotation
-	particle.emitting = true
-	get_tree().current_scene.add_child(particle)
-	self.queue_free()
+	pass # Replace with function body.
+
 
 func _on_re_calc_timer_timeout():
-	nav.target_position = player.global_position
+	pass # Replace with function body.
