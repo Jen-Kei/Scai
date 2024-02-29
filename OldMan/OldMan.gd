007e227e630c6cc9630415c95b0b981b2617780a
chubbyyb <keithsalhani7@gmail.com>
2024-02-29 22:56:14 +0000

    this works now

--- OldMan/OldMan.gd
+++ OldMan/OldMan.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@onready var AI = preload("res://UIPopup/UIPopup.tscn").instantiate()
+@onready var AI = preload("res://UIPopup/UIPopup.tscn")
 @onready var anim = $AnimationPlayer
 @onready var player = get_tree().get_root().get_child(0).get_node("Player")
 @onready var ekey = $eKey
@@ -9,17 +9,14 @@ extends CharacterBody2D
 func _ready():
 	anim.speed_scale = 0.5
 	anim.play("idle")
-	ekey.visible = true
+	ekey.visible = false
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if player.distance_to(self.position) < 50:
-		ekey.show = true
-		if Input.is_action_just_pressed("e"):
-			get_tree().get_root().add_child(AI)
-			AI.popup_centered = true
-			AI.popup_exclusive = true
-			AI.popup()
-	else:
+	if player.position.distance_to(self.position) < 50:
 		ekey.visible = true
+		if Input.is_action_just_pressed("interact"):
+			get_tree().get_root().add_child(AI.instantiate())
+	else:
+		ekey.visible = false
