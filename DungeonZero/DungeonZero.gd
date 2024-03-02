3ee1f167454e984613377004532210f26608987f
Jenny Thao Huynh C22448184 <c22448184@mytudublin.ie>
2024-03-02 17:09:57 +0000

    Main entrance transition through scenes done

--- DungeonZero/DungeonZero.gd
+++ DungeonZero/DungeonZero.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Globals.currentScene = "Forest"
+	Globals.currentScene = "DungeonZero"
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -11,17 +11,20 @@ func _process(delta):
 	change_scene()
 
 
-func _on_dungeon_transition_point_body_entered(body:Node2D):
+func _on_dungeon_zero_main_exit_body_entered(body:Node2D):
 	if body.has_method("player"):
 		Globals.transitionScene = true
 
 
-func _on_dungeon_transition_point_body_exited(body:Node2D):
+func _on_dungeon_zero_main_exit_body_exited(body:Node2D):
 	if body.has_method("player"):
 		Globals.transitionScene = false
 
 func change_scene():
 	if Globals.transitionScene == true:
-		if Globals.currentScene == "Forest":
-			get_tree().change_scene_to_file("res://DungeonZero/DungeonZero.tscn")
+		print("we in")
+		print(Globals.currentScene)
+		if Globals.currentScene == "DungeonZero":
+			print("in again")
+			get_tree().change_scene_to_file("res://Forest/Forest.tscn")
 			Globals.finish_change_scenes()
