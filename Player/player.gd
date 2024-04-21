bc591b0cd8e8469b2be91bdb7d0470285a2fee1a
Jenny Thao Huynh C22448184 <c22448184@mytudublin.ie>
2024-04-21 16:03:24 +0100

    Added player to be refactored

--- Player/player.gd
+++ Player/player.gd
@@ -1,44 +1,35 @@
-extends CharacterBody3D
+extends CharacterBody2D
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-
-@onready var animTree = $AnimationTree
-@onready var animState = animTree.get("parameters/playback")
+var input_movement = Vector2.ZERO
 
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
 
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	player_movement(delta)
 
-	# Get the input direction and handle the movement/deceleration.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+# Move the character and update animation
+func player_movement(delta):
+	var speed = 50
+	var animTree = $AnimationTree
+	var animState = animTree.get("parameters/playback")
+	input_movement = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+
+	if input_movement != Vector2.ZERO:
+		animTree.set("parameters/Idle/blend_position", input_movement)
+		animTree.set("parameters/Walk/blend_position", input_movement)
+
+		animState.travel("Walk")
+
+		velocity = input_movement * speed
 	
-	if direction:
-		# Set the blend position based on the input direction for AnimationTree
-		$AnimationTree.set("parameters/Idle/blend_position", input_dir)
-		$AnimationTree.set("parameters/Walk/blend_position", input_dir)
-		$AnimationTree.get("parameters/playback").travel("Walk")  # Switch to Walk animation
-
-		# Set velocity for movement
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		# If no movement input, switch to Idle animation
-		$AnimationTree.get("parameters/playback").travel("Idle")
+	if input_movement == Vector2.ZERO:
+		animState.travel("Idle")
+		velocity = Vector2.ZERO
 		
-		# Decelerate if no input direction
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
