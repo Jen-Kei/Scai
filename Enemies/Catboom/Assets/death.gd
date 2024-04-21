ece71ed31e340917f1b2ea45c0b74fe19baf0a08
Jenny Thao Huynh C22448184 <c22448184@mytudublin.ie>
2024-04-21 13:53:47 +0100

    initial commit of refactor

--- Enemies/Catboom/Assets/death.gd
+++
@@ -1,8 +0,0 @@
-extends GPUParticles2D
-
-@onready var timeCreated = Time.get_ticks_msec()
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	if Time.get_ticks_msec() - timeCreated > 10000:
-		queue_free()
